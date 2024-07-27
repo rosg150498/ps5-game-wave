e389d311ee9bbcc76042614091025c9580792081
rosg150498 <rosg150498@gmail.com>
2024-07-27 21:27:13 +0000

    Install django files and add gitignore file with content to file directory

---
+++ ps5_gamewave/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ps5_gamewave project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ps5_gamewave.settings')
+
+application = get_wsgi_application()
